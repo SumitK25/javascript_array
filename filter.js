1fe982ee6a57c4a1f1c75c8a162c572179766cd1
Sumit Kumar <86864217+SumitK25@users.noreply.github.com>
2022-07-13 07:18:36 +0530

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,19 +1,13 @@
-let cb = (element) => {
-  let a;
-  element < 5 ? (a = true) : (a = false);
-  return a;
-};
-
-const  filter=(elements, cb)=> {
-  ss= [];
-  for (let i = 0; i < elements.length; i++) {
-    let a = cb(elements[i]);
-    if (a) {
-      ss.push(elements[i]);
+function filter(elements, cb) {
+    if (!elements || !cb) {
+        return []
     }
-  }
-  return ss;
-
+    let arr = []
+    for (let index = 0; index < elements.length; index++) {
+        if (cb(elements[index])) {
+            arr.push(elements[index])
+        }
+    }
+    return arr;
 }
-module.exports.a = filter;
-module.exports.b = cb;
+module.exports = {filter}
