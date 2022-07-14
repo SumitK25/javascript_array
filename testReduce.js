5399359227b727a7cc5555bdf5f9d2ac3dd7d5ea
Sumit Kumar <86864217+SumitK25@users.noreply.github.com>
2022-07-14 07:45:15 +0530

    Update testReduce.js

--- testReduce.js
+++ testReduce.js
@@ -2,5 +2,5 @@ const reduceFile = require("./reduce.js")
 
 const items = [1, 2, 3, 4, 5, 5];
 
-result = reduceFile.reduce(items, (startingValue, CurrValue) => startingValue + CurrValue);
+const result = reduceFile(items, (accur, Curr) => accur + Curr);
 console.log(result);
